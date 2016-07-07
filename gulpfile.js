767a4bcc83d034fd94c6395f6e0d1a0c63b92212
John Newcomb <jnewcs@stanford.edu>
2016-07-07 12:11:17 -0500

    Adding main branch to contain folders before precompiling gulp steps

---
+++ gulpfile.js
@@ -0,0 +1,94 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var coffee = require('gulp-coffee');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+var notify = require("gulp-notify");
+var plumber = require('gulp-plumber');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var ghPages = require('gulp-gh-pages');
+
+// both used to serve static files as well as automatically reload browser on changes
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./dist",
+      serveStaticOptions: { // serves files without .html extension - thanks https://github.com/BrowserSync/browser-sync/issues/197#issuecomment-169704723
+        extensions: ['html']
+      }
+    },
+    notify: false // do not show a notification every time an update is done
+    // open: false // can be set so it does not automatically open browser
+  });
+});
+
+// compile sass to css
+gulp.task('sass', function () {
+  return gulp.src('./css/*.scss')
+    .pipe(plumber({errorHandler: notify.onError("Sass error: <%= error.message %>")}))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(concat('main.css'))
+    .pipe(gulp.dest('./dist'))
+    .pipe(browserSync.stream());
+});
+
+// CSS concat, auto-prefix and minify
+gulp.task('css', function() {
+  return gulp.src(['./css/*.css'])
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(concat('main.css'))
+    .pipe(gulp.dest('./dist'))
+    .pipe(browserSync.stream());
+});
+
+// compile all our js files into one file
+gulp.task('js', function() {
+  gulp.src('./js/*.js')
+    .pipe(concat("main.js"))
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist'))
+    .pipe(browserSync.stream());
+});
+
+// compile all our coffeescript files to js
+gulp.task('coffee', function() {
+  gulp.src('./coffee/*.coffee')
+    .pipe(plumber({errorHandler: notify.onError("CoffeeScript error: <%= error.message %>")}))
+    .pipe(coffee({bare: true}))
+    .pipe(concat("main.js"))
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist'))
+    .pipe(browserSync.stream());
+});
+
+// simply copy files to our output folder
+gulp.task('copy-extras', function() {
+  gulp.src('*.html')
+    .pipe(gulp.dest('./dist'))
+    .pipe(browserSync.stream());
+});
+
+// watch our files for changes
+gulp.task('watch', function () {
+	gulp.watch("*.html", ['copy-extras']);
+	gulp.watch("./js/*.js", ['js']);
+  gulp.watch('./css/*.css', ['css']);
+  gulp.watch('./css/*.scss', ['sass']);
+  gulp.watch('./coffee/*', ['coffee']);
+});
+
+// deploy to github pages automatically
+gulp.task('deploy', ['build'], function() {
+  return gulp.src('./dist/**/*')
+    .pipe(ghPages({
+      branch: "master" // deploy branch needs to be master, since this is a Github user site, not a project site
+    }));
+});
+
+// group together all the relevant 'build' tasks
+gulp.task('build', ['css', 'sass', 'js', 'coffee', 'copy-extras']);
+
+// by default, build everything, start the webserver, and watch our files for changes
+gulp.task('default', ['build', 'browser-sync', 'watch']);
---
+++ js/main.js
@@ -0,0 +1,76 @@
+// initialize EmailJS plugin
+emailjs.init("user_9XlP21UFyEIGPFx7mKChn");
+$("input").on("keyup", function(e) {
+	$(this).css("border-bottom", "2px solid #bebed2");
+	$(this).next().hide();
+});
+
+$("#send").on("click", function(e) {
+    NProgress.start();
+    var from_name = $("#from-name").val();
+    var email = $("#from-email").val();
+    var message = $("#from-message").val();
+
+    if(validFields(from_name, email, message)) {
+    	sendEmail(from_name, email, message);
+    } else {
+    	// show errors
+    	showErrors();
+    }
+  });
+
+function validFields(name, email, message) {
+	if(name.length === 0) {
+		$("#from-name").css("border-bottom", "2px solid #c40022");
+		$("#from-name-error").show();
+	} else {
+		$("#from-name-error").hide();
+	}
+
+	if(email.length === 0) {
+		$("#from-email").css("border-bottom", "2px solid #c40022");
+		$("#from-email-error").show();
+	} else {
+		$("#from-email-error").hide();
+	}
+
+	if(message.length === 0) {
+		$("#from-message").css("border-bottom", "2px solid #c40022");
+		$("#from-message-error").show();
+	} else  {
+		$("#from-message-error").hide();
+	}
+	return false;
+}
+
+function showErrors() {
+	NProgress.done();
+	console.log("Errors found");
+}
+
+function sendEmail(from_name, contact_email, message) {
+	emailjs.send("yahoo1453","template_vqIpQ2sP", {from_name: from_name, contact_email: contact_email, message_html: message})
+		.then(function(response) {
+	   console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
+	   NProgress.done();
+	}, function(err) {
+	   console.log("FAILED. error=", err);
+	});
+
+	console.log("Dirk");
+};
+
+(function() {
+	var textarea = document.querySelector('textarea');
+
+	textarea.addEventListener('keydown', autosize);
+	             
+	function autosize(){
+	  var el = this;
+	  console.log("Here");
+	  el.style.cssText = 'height:auto; padding:0';
+    // for box-sizing other than "content-box" use:
+    // el.style.cssText = '-moz-box-sizing:content-box';
+    el.style.cssText = 'height:' + el.scrollHeight + 'px';
+	}
+})();
